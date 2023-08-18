9ec9788f335c15374a7d0343dcd35ee57ccf4edd
FINIKKKK <digitallkrash@gmail.com>
2023-08-18 14:23:43 +0600

    main screen pt2

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 // WEBP
-import { isWebp } from "./function.js";
+import {isWebp} from "./function.js";
+import Swiper from 'swiper';
+
 isWebp();
 
 
+const sliderBanners = new Swiper('.banners', {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 100,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: true,
+    },
+});

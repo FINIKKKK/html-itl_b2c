98053b0208e74e883c474278b1965b4232e6e910
FINIKKKK <digitallkrash@gmail.com>
2023-08-19 12:30:50 +0600

    sidebar, price range

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,10 @@ import {isWebp} from "./function.js";
 
 isWebp();
 
-
 import Swiper from "swiper";
 import {Navigation, Pagination, EffectFade, Autoplay} from 'swiper/modules';
 
+// Slider
 const sliderProducts = new Swiper('.products', {
     modules: [Navigation],
     slidesPerView: 6,
@@ -18,6 +18,7 @@ const sliderProducts = new Swiper('.products', {
     },
 });
 
+// Slider
 const sliderBanners = new Swiper('.banners', {
     modules: [Pagination, EffectFade, Autoplay],
     slidesPerView: 1,
@@ -36,6 +37,7 @@ const sliderBanners = new Swiper('.banners', {
     },
 });
 
+// Slider
 const sliderDay = new Swiper('.day', {
     modules: [Pagination, EffectFade, Autoplay],
     slidesPerView: 1,
@@ -54,3 +56,44 @@ const sliderDay = new Swiper('.day', {
         disableOnInteraction: true,
     },
 });
+
+
+// Price Range
+const lowerSlider = document.querySelector('#lower');
+const upperSlider = document.querySelector('#upper');
+
+document.querySelector('#two').value = upperSlider.value;
+document.querySelector('#one').value = lowerSlider.value;
+
+let lowerVal = parseInt(lowerSlider.value);
+let upperVal = parseInt(upperSlider.value);
+
+lowerSlider.max = 140900;
+upperSlider.max = 140900;
+upperSlider.value = upperSlider.max
+document.querySelector('#two').value = upperSlider.max
+
+upperSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+
+    if (upperVal < lowerVal + 4) {
+        lowerSlider.value = upperVal - 4;
+        if (lowerVal == lowerSlider.min) {
+            upperSlider.value = 4;
+        }
+    }
+    document.querySelector('#two').value = this.value
+};
+
+lowerSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+    if (lowerVal > upperVal - 4) {
+        upperSlider.value = lowerVal + 4;
+        if (upperVal == upperSlider.max) {
+            lowerSlider.value = parseInt(upperSlider.max) - 4;
+        }
+    }
+    document.querySelector('#one').value = this.value
+};
